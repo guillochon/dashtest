023f1f6551e253464bf268c535d9bae239d9bd1a
guillochon <guillochon@gmail.com>
2017-11-18 12:34:18 -0500

    working nicely now as a demo

--- dashtest.py
+++ dashtest.py
@@ -1,11 +1,27 @@
+"""Test dash out on OSC data."""
+from collections import OrderedDict
 import dash
+import json
 import numpy as np
+import urllib.request
 
-wave = np.arange(2500, 10000, 2)
-flux = np.ones(len(wave))
-filename = np.array([wave, flux])
+event_name = 'SN2002er'
 
-classification = dash.Classify([filename], [0.], classifyHost=False, rlapScores=True)
-bestFits, redshifts, bestTypes, rlapFlag, matchesFlag = classification.list_best_matches(n=5)
+url = "https://api.sne.space/" + event_name + "/spectra/time+data?item=10"
+response = urllib.request.urlopen(url)
+data = json.loads(response.read(), object_pairs_hook=OrderedDict)
+
+data = data[next(iter(data))]['spectra'][0][1]
+data = np.array(list(map(list, zip(*data)))).astype(np.float)
+
+url = "https://api.sne.space/" + event_name + "/redshift/value"
+response = urllib.request.urlopen(url)
+redshift = json.loads(response.read(), object_pairs_hook=OrderedDict)
+redshift = float(redshift[next(iter(redshift))]['redshift'][0][0])
+
+classification = dash.Classify(
+    [data], [redshift], classifyHost=False, rlapScores=True)
+(bestFits, redshifts, bestTypes, rlapFlag,
+    matchesFlag) = classification.list_best_matches(n=5)
 
 print(bestFits)
